5540d563d47a8affcad6fbbd82701e788c569894
dmitrylobov <dilo1992.edu@gmail.com>
2022-11-01 23:44:10 +0300

    Homework for a lesson 24

---
+++ src/main/HW24/User.java
@@ -0,0 +1,11 @@
+package main.HW24;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private String login;
+    private String password;
+}
